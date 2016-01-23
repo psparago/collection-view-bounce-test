46b28d261c0092223fd960ba7a58c37f4f5daf4b
peter <peter.sparago@gmail.com>
2016-01-23 17:25:04 -0500

    Initial Commit

---
+++ collection-view-bounce-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  collection-view-bounce-test
+//
+//  Created by Peter Sparago on 1/23/16.
+//  Copyright © 2016 Angelware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
